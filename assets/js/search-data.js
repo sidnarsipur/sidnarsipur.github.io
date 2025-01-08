003feffd4aeede30d7e1a990c19dca08d3a60d3e
sidnarsipur <72701664+sidnarsipur@users.noreply.github.com>
2025-01-08 09:24:32 +0000

    Deploying to gh-pages from @ sidnarsipur/sidnarsipur.github.io@dde617a3206eed55c2c36a88eb918984814d1d4b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -406,6 +406,11 @@ ninja.data = [{
           description: "Twine Game",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-a-day-of-screens-and-swipes",
+          title: 'A Day of Screens and Swipes',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{
         id: 'social-github',
         title: 'GitHub',
