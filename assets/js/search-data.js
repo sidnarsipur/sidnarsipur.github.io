9fc2af2d1a75e3b8f1c0ac4fa439940f1487c0d0
sidnarsipur <sid.narsipur@gmail.com>
2025-02-27 05:17:30 +0000

    Deploying to gh-pages from @ sidnarsipur/sidnarsipur.github.io@a2fb056c678b279f15becddcdd56debe8d803249 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
+  },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "nav-projects",
           title: "projects",
           description: "This is a (growing) list of projects I&#39;ve worked on.",
           section: "Navigation",
